6bcad61b00b711ec7a9f597d37cc7a140721c62b
amitrajput921998 <pamit8327@gmail.com>
2023-06-11 19:42:32 +0530

    first commit

---
+++ readme.md
@@ -0,0 +1,39 @@
+# Weather App
+
+The Weather App is a simple web application that allows users to search for weather information based on city names. It retrieves real-time weather data using the WeatherAPI and displays the weather details for the specified city.
+
+## Features
+
+- Search for weather information by entering a city name.
+- Displays the country, state, and city name.
+- Shows the current weather conditions, including humidity, wind speed, and temperature.
+- Includes an animated cloud background for visual appeal.
+
+## How to Use
+
+1. Clone the repository or download the source code.
+
+2. Obtain an API key from the WeatherAPI website. (Replace `YOUR_API_KEY` in the code with your actual API key.)
+
+3. Open the `index.html` file in a web browser.
+
+4. Enter a city name in the input box and click the "Search" button.
+
+5. The weather information for the specified city will be displayed on the page.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+## Dependencies
+
+- WeatherAPI (Get your API key from [WeatherAPI](https://www.weatherapi.com/))
+
+## Output
+![](./img/outputw.PNG)
+
+## License
+
+The Weather App is licensed under the [MIT License](LICENSE).
---
+++ script.js
@@ -0,0 +1,45 @@
+let data;
+
+const inputBox = document.getElementById("inputBox");
+const countryName = document.getElementById("countryName");
+const stateName = document.getElementById("stateName");
+const cityName = document.getElementById("cityName");
+const humidity = document.getElementById("humidity");
+const windSpeed = document.getElementById("windSpeed");
+const temperature = document.getElementById("temperature");
+const logoImage = document.getElementById("logoImage");
+const weatherStatus = document.getElementById("weatherStatus");
+
+const getData = async (event) => {
+  event.preventDefault();
+
+  if (!inputBox.value) {
+    alert("Please enter the city name");
+    return;
+  }
+
+  const city = inputBox.value;
+
+  try {
+    const fetchData = await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=aec03013669e4e15bb563220231106&q=${city}`
+    );
+    const orgData = await fetchData.json();
+
+    data = orgData;
+    console.log(data);
+
+    // Display the data in HTML
+    countryName.textContent = data.location.country;
+    stateName.textContent = data.location.region;
+    cityName.textContent = data.location.name;
+    logoImage.src = data.current.condition.icon;
+    weatherStatus.textContent = data.current.condition.text;
+    humidity.textContent = `${data.current.humidity}%`;
+    windSpeed.textContent = `${data.current.wind_kph} KMPH`;
+    temperature.textContent = `${data.current.temp_c} \u2103`; // Display temperature in Celsius
+  } catch (error) {
+    console.log("Error fetching data:", error);
+  }
+};
+
