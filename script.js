f48ee69b434e799ad0211e528e163af1520aeba1
amitrajput921998 <pamit8327@gmail.com>
2023-06-11 20:04:32 +0530

    change

--- script.js
+++ script.js
@@ -1,45 +1,36 @@
-let data;
-
-const inputBox = document.getElementById("inputBox");
-const countryName = document.getElementById("countryName");
-const stateName = document.getElementById("stateName");
-const cityName = document.getElementById("cityName");
-const humidity = document.getElementById("humidity");
-const windSpeed = document.getElementById("windSpeed");
-const temperature = document.getElementById("temperature");
-const logoImage = document.getElementById("logoImage");
-const weatherStatus = document.getElementById("weatherStatus");
-
-const getData = async (event) => {
-  event.preventDefault();
-
-  if (!inputBox.value) {
-    alert("Please enter the city name");
-    return;
-  }
-
-  const city = inputBox.value;
-
-  try {
-    const fetchData = await fetch(
-      `http://api.weatherapi.com/v1/current.json?key=aec03013669e4e15bb563220231106&q=${city}`
-    );
-    const orgData = await fetchData.json();
-
-    data = orgData;
-    console.log(data);
-
-    // Display the data in HTML
-    countryName.textContent = data.location.country;
-    stateName.textContent = data.location.region;
-    cityName.textContent = data.location.name;
-    logoImage.src = data.current.condition.icon;
-    weatherStatus.textContent = data.current.condition.text;
-    humidity.textContent = `${data.current.humidity}%`;
-    windSpeed.textContent = `${data.current.wind_kph} KMPH`;
-    temperature.textContent = `${data.current.temp_c} \u2103`; // Display temperature in Celsius
-  } catch (error) {
-    console.log("Error fetching data:", error);
-  }
-};
-
+const fetchData = async (city) => {
+    try {
+      const response = await fetch(`/.netlify/functions/weather-api?city=${city}`);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.log("Error fetching data:", error);
+      return null;
+    }
+  };
+  
+  // Update the getData function
+  const getData = async (event) => {
+    event.preventDefault();
+  
+    if (!inputBox.value) {
+      alert("Please enter the city name");
+      return;
+    }
+  
+    const city = inputBox.value;
+    const data = await fetchData(city);
+  
+    if (data) {
+      // Display the data in HTML
+      countryName.textContent = data.location.country;
+      stateName.textContent = data.location.region;
+      cityName.textContent = data.location.name;
+      logoImage.src = data.current.condition.icon;
+      weatherStatus.textContent = data.current.condition.text;
+      humidity.textContent = `${data.current.humidity}%`;
+      windSpeed.textContent = `${data.current.wind_kph} KMPH`;
+      temperature.textContent = `${data.current.temp_c} \u2103`; // Display temperature in Celsius
+    }
+  };
+  
