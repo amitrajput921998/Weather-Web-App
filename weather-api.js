9d80335464ccb2d523983c577d3d2279678994b2
amitrajput921998 <pamit8327@gmail.com>
2023-06-11 19:55:15 +0530

    add file

---
+++ weather-api.js
@@ -0,0 +1,22 @@
+const fetch = require('node-fetch');
+
+exports.handler = async (event, context) => {
+  const { city } = event.queryStringParameters;
+  const apiKey = 'YOUR_WEATHER_API_KEY';
+  const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${aec03013669e4e15bb563220231106}&q=${city}&aqi=no`;
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Internal server error' }),
+    };
+  }
+};
